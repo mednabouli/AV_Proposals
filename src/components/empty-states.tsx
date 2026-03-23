803f2b2a46b54cec570b2e858f2505bcb1355e15
med <mednabouli@gmail.com>
2026-03-23 16:34:10 -0400

    feat: adjust cta

--- src/components/empty-states.tsx
+++ src/components/empty-states.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "./ui/button-variants";
 import { FileText } from "lucide-react";
 import { cn } from "@/lib/utils";
 
